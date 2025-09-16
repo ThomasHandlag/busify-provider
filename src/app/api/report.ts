7472fef5f3b0b3803e7ed76408a717eeea90bcb2
unknown <truongthuongvan203@gmail.com>
2025-09-16 15:04:14 +0700

    feat: report charts

--- src/app/api/report.ts
+++ src/app/api/report.ts
@@ -9,3 +9,37 @@ export async function getWeeklyOperatorReport(oId: number): Promise<{
   const response = await apiClient.get(`api/bus-operators/${oId}/report`);
   return response.data.result;
 }
+
+export interface ReportResponse {
+  id: string;
+  title: string;
+  reportDate: Date;
+  operatorId: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Record<string, any>;
+}
+
+export async function getReportYearly(
+  operatorId: number,
+  year: number
+): Promise<ReportResponse[]> {
+  const response = await apiClient.get(
+    `api/reports/yearly?operatorId=${operatorId}&year=${year}`
+  );
+  return response.data.result;
+}
+
+export async function getReportByYearRange({
+  start,
+  end,
+  id,
+} : {
+  start: number;
+  end: number;
+  id: number;
+}): Promise<ReportResponse[]> {
+  const response = await apiClient.get(
+    `api/reports/year-range?start=${start}&end=${end}&operatorId=${id}`
+  );
+  return response.data.result;
+}
--- src/features/report/report.tsx
+++ src/features/report/report.tsx
@@ -1,62 +1,122 @@
-import { Card, Row, Col, Typography } from "antd";
-import { Pie, Bar, Line } from "@ant-design/charts";
+import { Card, Row, Col, Typography, Empty } from "antd";
+import { Bar, Line } from "@ant-design/charts";
+import React, { useEffect } from "react";
+import { getReportYearly } from "../../app/api/report";
+import { operatorStore } from "../../stores/operator_store";
 
 const { Title } = Typography;
 
-// Dữ liệu mẫu
-const tripStatusData = [
-  { type: "Đã lên lịch", value: 40 },
-  { type: "Đúng giờ", value: 30 },
-  { type: "Bị hoãn", value: 10 },
-  { type: "Đã khởi hành", value: 15 },
-  { type: "Đã đến nơi", value: 20 },
-  { type: "Đã hủy", value: 5 },
-];
+type ChartData = {
+  [key: string]: string | number;
+};
 
-const employeeStatusData = [
-  { type: "Hoạt động", value: 60 },
-  { type: "Ngừng hoạt động", value: 25 },
-  { type: "Bị cấm", value: 5 },
-];
-
-const revenueData = [
-  { month: "Tháng 1", revenue: 120 },
-  { month: "Tháng 2", revenue: 150 },
-  { month: "Tháng 3", revenue: 180 },
-  { month: "Tháng 4", revenue: 90 },
-  { month: "Tháng 5", revenue: 200 },
-  { month: "Tháng 6", revenue: 170 },
-];
+export const RandomColor = (): string => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 const ReportPage = () => {
-  const pieConfig = {
-    appendPadding: 10,
-    data: tripStatusData,
-    angleField: "value",
-    colorField: "type",
-    radius: 0.8,
+  const [revenueData, setRevenueData] = React.useState<ChartData[]>([]);
+  const [tripsData, setTripsData] = React.useState<ChartData[]>([]);
+  const [ratioData, setRatioData] = React.useState<ChartData[]>([]);
+  const operator = operatorStore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (operator?.operator?.id) {
+        const reportData = await getReportYearly(
+          operator?.operator?.id || 0,
+          new Date().getFullYear()
+        );
+        console.log(reportData);
+        reportData
+          .sort(
+            (a, b) =>
+              new Date(a.reportDate).getTime() -
+              new Date(b.reportDate).getTime()
+          )
+          .forEach((item) => {
+            const reportMonth = new Date(item.reportDate);
+            const revenue = item.data.totalRevenue;
+            const tripsCount = item.data.totalTrips ?? 0;
+            const cancelRatio = item.data.cancelTicketRatio ?? 0;
+
+            setRevenueData((prev) => [
+              ...prev,
+              {
+                month: reportMonth.toLocaleString("vi-VN", { month: "long" }),
+                revenue: Number(revenue),
+              },
+            ]);
+            setTripsData((prev) => [
+              ...prev,
+              {
+                month: reportMonth.toLocaleString("vi-VN", { month: "long" }),
+                tripsCount,
+              },
+            ]);
+            setRatioData((prev) => [
+              ...prev,
+              {
+                month: reportMonth.toLocaleString("vi-VN", { month: "long" }),
+                cancelRatio,
+                usedRatio: 100 - cancelRatio,
+              },
+            ]);
+          });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const cancelRatioPie = {
+    data: ratioData,
+    xField: "month",
+    yField: "cancelRatio",
+    colorField: "month",
+    color: "#1890ff",
     label: {
-      type: "outer",
-      content: "{name} {percentage}",
+      style: { fill: "#FFFFFF", opacity: 0.6 },
     },
-    interactions: [{ type: "element-active" }],
+    xAxis: { min: 0 },
   };
 
-  const barConfig = {
-    data: employeeStatusData,
-    xField: "value",
-    yField: "type",
-    seriesField: "type",
-    legend: { position: "top-left" },
-    color: ["#52c41a", "#ff974d", "#ff4d4f"],
+  const tripsBars = {
+    data: tripsData,
+    xField: "month",
+    yField: "tripsCount",
+    color: "#1890ff",
+    label: {
+      style: { fill: "#FFFFFF", opacity: 0.6 },
+    },
+    xAxis: { min: 0 },
   };
 
-  const lineConfig = {
+  const revenueLine = {
     data: revenueData,
     xField: "month",
     yField: "revenue",
-    smooth: true,
-    label: {},
+    shapeField: "smooth",
+    scale: {
+      y: {
+        domainMin: 0,
+      },
+    },
+    interaction: {
+      tooltip: {
+        marker: false,
+      },
+    },
+    // smooth: true,
+    label: {
+      style: { fill: "#aaa" },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      formatter: (v: any) => `${v}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    },
     point: { size: 4, shape: "circle" },
     color: "#1890ff",
   };
@@ -67,19 +127,34 @@ const ReportPage = () => {
         Báo cáo tổng hợp
       </Title>
       <Row gutter={24}>
-        <Col xs={24} md={12} xl={8}>
-          <Card title="Tỷ lệ trạng thái chuyến xe" style={{ marginBottom: 24 }}>
-            <Pie {...pieConfig} />
+        <Col span={24} style={{ marginBottom: 24 }}>
+          <Card title="Số vé hoàn trả mỗi tháng" style={{ marginBottom: 24 }}>
+            {ratioData.length > 0 ? (
+              <Bar {...cancelRatioPie} />
+            ) : (
+              <Empty description="Chưa có dữ liệu" />
+            )}
           </Card>
         </Col>
-        <Col xs={24} md={12} xl={8}>
-          <Card title="Nhân viên theo trạng thái" style={{ marginBottom: 24 }}>
-            <Bar {...barConfig} />
+        <Col span={24}>
+          <Card
+            title="Các chuyến đã hoàn thành trong tháng"
+            style={{ marginBottom: 24 }}
+          >
+            {tripsData.length > 0 ? (
+              <Bar {...tripsBars} />
+            ) : (
+              <Empty description="Chưa có dữ liệu" />
+            )}
           </Card>
         </Col>
-        <Col xs={24} xl={8}>
+        <Col span={24}>
           <Card title="Doanh thu theo tháng" style={{ marginBottom: 24 }}>
-            <Line {...lineConfig} />
+            {revenueData.length > 0 ? (
+              <Line {...revenueLine} />
+            ) : (
+              <Empty description="Chưa có dữ liệu" />
+            )}
           </Card>
         </Col>
       </Row>
